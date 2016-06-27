13d3f226d518be135ce62915f1ed3d96db0adfe3
brettwgreen <bw_green@yahoo.com>
2016-06-27 15:51:38 -0400

    Switch to conenct web server

--- src/gruntfile.js
+++ src/gruntfile.js
@@ -3,21 +3,20 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    web_server: {
+    connect: {
+      server: {
         options: {
-          cors: true,
           port: 8000,
-          nevercache: true,
-          logRequests: true
-        },
-        foo: 'bar' // For some reason an extra key with a non-object value is necessary 
-      }    
+          keepalive: true
+        }
+      }
+    }   
   });
 
   // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-web-server');
+  grunt.loadNpmTasks('grunt-contrib-connect');
 
   // Default task(s).
-  grunt.registerTask('default', ['web_server']);
+  grunt.registerTask('default', ['connect']);
 
 };
